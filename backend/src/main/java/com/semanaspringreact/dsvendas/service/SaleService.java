e7fd8077293d8f13492ffa5e10ca4f121e0c1f85
Kagi Adrian Garcia de Carvalho <kagi.carvalho@outlook.com>
2021-05-05 17:00:39 -0300

    Include Group by search

--- backend/src/main/java/com/semanaspringreact/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/semanaspringreact/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.semanaspringreact.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.semanaspringreact.dsvendas.dto.SaleDTO;
+import com.semanaspringreact.dsvendas.dto.SaleSuccessDTO;
+import com.semanaspringreact.dsvendas.dto.SaleSumDTO;
 import com.semanaspringreact.dsvendas.entities.Sale;
 import com.semanaspringreact.dsvendas.repositories.SaleRepository;
 import com.semanaspringreact.dsvendas.repositories.SellerRepository;
@@ -27,5 +31,14 @@ public class SaleService {
 		
 		return result.map(x -> new SaleDTO(x));		
 	}
-		
+
+	@Transactional(readOnly = true)		
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupBySeller();
+	}
+
+	@Transactional(readOnly = true)		
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	}	
 }
