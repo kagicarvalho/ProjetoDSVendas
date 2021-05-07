64c69747b4d212675fed923f77abbaac0d742c53
Kagi Adrian Garcia de Carvalho <kagi.carvalho@outlook.com>
2021-05-07 14:06:29 -0300

    Include routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,24 @@
+import Dashborad from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+
+                <Route path="/dashboard">
+                    <Dashborad />
+                </Route>
+
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+
+export default Routes;
